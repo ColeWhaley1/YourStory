16c0a727b452c649281e822e9f1d402925c6268c
ColeWhaley1 <colewhaley1@gmail.com>
2025-01-12 18:56:31 -0500

    move signInWithPassword to frontend

--- src/frontend/services/signInUser.ts
+++ src/frontend/services/signInUser.ts
@@ -1,4 +1,5 @@
 import { SignIn } from "../../types/story";
+import supabase from "../supabase";
 
 interface SignInResponse {
     id: string | null;
@@ -8,33 +9,23 @@ interface SignInResponse {
 const signInUser = async (sign_in_info: SignIn, allowAccess: () => void): Promise<SignInResponse> => {
     try {
 
-        const base_url = import.meta.env.VITE_API_BASE_URL
-
-        const response = await fetch(`${base_url}/sign_in`, {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify({ email: sign_in_info.email, password: sign_in_info.password }),
+        const { data, error } = await supabase.auth.signInWithPassword({
+            email: sign_in_info.email, 
+            password: sign_in_info.password
         });
-        
-        const data = await response.json();
 
-        if (!response.ok) {
-            if (data.error == "Invalid login credentials") {
-                throw new Error(data.error);
-            }
-            throw new Error("Failed to sign in user");
+        if (error){
+            throw new Error(error.message);
         }
 
         allowAccess();
 
-        return { 
-            id: data.id,
-            error: data.error
-        };
+        return {
+            id: data.user.id,
+            error: null
+        }
+
     } catch (error: any) {
-        console.error(error.message);
         return {
             id: null,
             error: error.message
