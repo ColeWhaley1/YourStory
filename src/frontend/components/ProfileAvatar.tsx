4b95c7280f9f3800e3553d289fddaaef5653e16c
ColeWhaley1 <colewhaley1@gmail.com>
2025-01-23 17:42:23 -0500

    fix avatar loading

--- src/frontend/components/ProfileAvatar.tsx
+++ src/frontend/components/ProfileAvatar.tsx
@@ -1,4 +1,6 @@
+import { useState } from "react";
 import DefaultAvatar from "../../assets/static_images/DefaultAvatar.png";
+import Loading from "./Loading";
 
 interface ProfileAvatarProps {
   avatarUrl?: string | null;
@@ -6,12 +8,18 @@ interface ProfileAvatarProps {
 }
 
 const ProfileAvatar: React.FC<ProfileAvatarProps> = ({ avatarUrl, size="w-64 h-64" }) => {
+  const [loading, setLoading] = useState<boolean>(true);
+
   return (
     <div className={`flex items-center justify-center rounded-full overflow-hidden ${size}`}>
+      <div className={`${loading ? "visible" : "hidden"}`}>
+        <Loading/>
+      </div>
       <img
         src={avatarUrl ?? DefaultAvatar}
         alt="Profile Avatar"
-        className="w-full h-full object-cover"
+        className={`w-full h-full object-cover ${!loading ? "visible" : "hidden"}`}
+        onLoad={() => setLoading(false)}
       />
     </div>
   );
