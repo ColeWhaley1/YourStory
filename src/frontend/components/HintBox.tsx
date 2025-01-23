937b536e345850d38160691b2e17e77571fa9fda
ColeWhaley1 <colewhaley1@gmail.com>
2025-01-23 14:00:02 -0500

    fix hintBox UI

--- src/frontend/components/HintBox.tsx
+++ src/frontend/components/HintBox.tsx
@@ -11,9 +11,9 @@ interface HintBoxProps {
 const HintBox: React.FC<HintBoxProps> = ({ message, header, setShowHint, exitButtonTitle = "Continue" }) => {
     return (
         <div className="absolute inset-0 -top-48 flex items-center justify-center">
-            <div className="flex flex-col items-center justify-center min-w-[8rem] min-h-[8rem] max-w-[24rem] bg-white shadow-lg z-10 border border-gray-300 rounded-lg p-6 text-gray-700 space-y-4">
+            <div className="flex flex-col items-center justify-center min-w-[8rem] min-h-[8rem] max-w-[28rem] bg-white shadow-lg z-10 border border-gray-300 rounded-lg p-6 text-gray-700 space-y-4">
                 <div className="flex items-center justify-center w-full bg-gray-100 p-2 rounded-md">
-                    <h3 className="text-xl">{header}</h3>
+                    <h3 className="text-2xl font-bold">{header}</h3>
                 </div>
                 <p className="text-center text-lg font-medium">{message}</p>
                 <button className="w-fit text-black text-center bg-secondary p-1 px-2 rounded-sm" onClick={() => setShowHint(false)}>
