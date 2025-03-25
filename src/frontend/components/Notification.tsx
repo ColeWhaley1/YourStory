71948612816754dbb2ad499b8658ab1f378fe3b1
ColeWhaley1 <colewhaley1@gmail.com>
2025-03-25 16:18:12 -0400

    get animation working

--- src/frontend/components/Notification.tsx
+++ src/frontend/components/Notification.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import { NotificationType } from "../contexts/notificationContext";
 import { IoCloseOutline } from "react-icons/io5";
 
@@ -11,6 +12,17 @@ interface NotificationProps {
 }
 
 const Notification: React.FC<NotificationProps> = ({ isVisible, setIsVisible, notificationType, message, lifespan, action }) => {
+
+    const [isExiting, setIsExiting] = useState<boolean>(false);
+    const [isEntering, setIsEntering] = useState<boolean>(true);
+
+    useEffect(() => {
+        const notificationEnteringTimeout = setTimeout(() => {
+            setIsEntering(false);
+        }, 700);
+
+        return () => clearTimeout(notificationEnteringTimeout);
+    })
 
     const getNotificationStyle = () => {
         switch (notificationType) {
@@ -43,8 +55,9 @@ const Notification: React.FC<NotificationProps> = ({ isVisible, setIsVisible, no
             case "error":
                 return (
                     <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 shrink-0 stroke-current" fill="none" viewBox="0 0 24 24">
-                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z" />
+                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z" />
                     </svg>
+
                 );
             default:
                 return (
@@ -55,8 +68,26 @@ const Notification: React.FC<NotificationProps> = ({ isVisible, setIsVisible, no
         }
     }
 
+    const onCloseNotification = () => {
+        setIsExiting(true);
+
+        setTimeout(() => {
+            setIsVisible(false);
+        }, 3000)
+    }
+
     return (
-        <div className="absolute top-4 w-full flex justify-center">
+        <div className={`absolute top-4 w-full flex justify-center z-50 ${isEntering
+                ?
+                "transform -translate-y-40 transition-transform duration-300"
+                :
+                isExiting
+                    ?
+                    "transform -translate-y-40 transition-transform duration-700"
+                    :
+                    "transform translate-y-0 transition-transform duration-300"
+            }`
+        }>
             <div className="opacity-90">
                 <div className={getNotificationStyle()}>
 
@@ -68,13 +99,13 @@ const Notification: React.FC<NotificationProps> = ({ isVisible, setIsVisible, no
                         {message}
                     </div>
 
-                    <div className="hover:scale-150 transition-transform duration-300 transform will-change-transform">
-                        <IoCloseOutline />
-                    </div>
+                    <button className="hover:scale-150 transition-transform duration-300" onClick={onCloseNotification}>
+                        <IoCloseOutline size={18} />
+                    </button>
 
                 </div>
             </div>
-        </div>
+        </div >
     )
 }
 
